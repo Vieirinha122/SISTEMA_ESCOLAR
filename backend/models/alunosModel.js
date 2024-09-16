80e8ee5bc50b9cb2df69cdb51fb5ddfead47d76a
Arthur Vieira <arthurv720@gmail.com>
2024-09-16 15:49:53 -0300

    - dados do banco alunoModel alterados

--- backend/models/alunosModel.js
+++ backend/models/alunosModel.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const StudentSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    senha: { type: String, required: true },
-    classes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Class' }] // exemplo pra referenciar a outro schema
+    senha: { type: String, required: true } // exemplo pra referenciar a outro schema
   });
   
   module.exports = mongoose.model('Student', StudentSchema);
