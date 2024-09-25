b8847dd10197b92c791b4c00c9d44ec5cb1216ce
Arthur Vieira <arthurv720@gmail.com>
2024-09-24 21:59:00 -0300

    - adicionado um arquivo .env de exemplo. - entidade de comunicado testado e aprovado - páginas só faltam ser estilizadas. - rotas funcionando normalmente.

--- backend/models/comunicadosModel.js
+++ backend/models/comunicadosModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const ComunicadoSchema = new mongoose.Schema({
     titulo: { type: String, required: true, trim: true},
+    id_turma: { type: mongoose.Schema.Types.ObjectId, ref: 'Turma'},
+    id_professor: { type: mongoose.Schema.Types.ObjectId, ref: 'Professor'},
     descricao: { type: String, required: true, trim: true},
     data: { type: Date, required: true, default: Date.now},
   });
